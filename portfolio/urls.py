07782ec54ce1075e71d1e5dd109334961c328c7b
Sylwenides <sylwesterstanio@gmail.com>
2022-09-18 01:40:33 +0300

    adding Math Game done with JS and html

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,5 +22,6 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('MathGame/', jobs.views.MathGame, name='MathGame'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
